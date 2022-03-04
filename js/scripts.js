b3bd9caa303d8d31e2884b36952ebef2ceb35622
Maureen-Muriithi <moh2wanja@gmail.com>
2022-03-04 22:15:06 +0300

    Link js file with html

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+// Declaring inputs for the user
+var date = 
+var month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+ month = function inputmonth;
+ 
