eebea11b6169498488732f35cc168bb37dfd8a1d
Maureen-Muriithi <moh2wanja@gmail.com>
2022-03-04 22:49:16 +0300

    Declare variables for the inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 // Declaring inputs for the user
-var date = 
-var month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
- month = function inputmonth;
- 
+var date = document.getElementById(inputDate)
+var month = document.getElementById(inputMonth)
+var year = document.getElementById(inputYear)
+var submitbtn = document.getElementById(submitbtn)
+
+// var month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+submitbtn.addEventListener('click'() => {
+
+}
