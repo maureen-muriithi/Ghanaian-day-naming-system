57d054cf3c85b405d4fd4b78929d5e90515c1b45
Maureen-Muriithi <moh2wanja@gmail.com>
2022-03-05 02:39:45 +0300

    Add arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,27 @@
 // Declaring inputs for the user
-var date = document.getElementById(inputDate)
-var month = document.getElementById(inputMonth)
-var year = document.getElementById(inputYear)
-var submitbtn = document.getElementById(submitbtn)
+var firstName =document.getElementById(inputFirstName);
+var lastName =document.getElementById(inputLastName);
+var date = document.getElementById(inputDate);
+var month = document.getElementById(inputMonth);
+var year = document.getElementById(inputYear);
+var submitbtn = document.getElementById(submitbtn);
 
-// var month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+var date = new date (document.getElementById(inputDate).value)
+var dayOfWeek = date.getDay()
+var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+var femNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 
-submitbtn.addEventListener('click'() => {
 
-}
+function printResult() {
+    var message = "Hello!: "+firstName+ +lastName+
+    "</br> You were born in: "+dayOfWeek+
+    "</br>Your Akan name is: " +AkanName+
+    document.getElementById('placeholder').innerHTML == message;
+};
+
+
+// submitbtn.addEventListener('click'() => {
+//     document.getElementById(message).innerHTML
+// }
