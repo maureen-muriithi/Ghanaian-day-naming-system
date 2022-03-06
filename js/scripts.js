f82645327c3649eba5b07c6f6767f47eaac93a82
Maureen-Muriithi <moh2wanja@gmail.com>
2022-03-06 18:51:38 +0300

    Add submit logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
-'use strict';
+// 'use strict';
 
+const submitButton = document.getElementById("submitbtn");
+submitButton.addEventListener('click', getAkanName);
 
-var errorCount = 0
-var errorData = {}
+const refreshButton = document.getElementById("refreshbtn");
+refreshButton.addEventListener('click', refreshForm)
 
 function getAkanName(){
 
@@ -10,20 +12,12 @@ function getAkanName(){
     var firstName =document.getElementById("inputFirstName").value;
     var lastName =document.getElementById("inputLastName").value;
     var birthDate = document.getElementById("birthDate").value;
-    var userGender = document.getElementById("CustomSelectGender");
+    var userGender = document.getElementById("CustomSelectGender").value;
 
     //required arrays for the days and Akan Names
     var femNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
     var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
     var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-
-    //buttons to be used in the form
-    var submitbtn = document.getElementById("submitbtn");
-    var refreshbtn = document.getElementById("refreshbtn")
-
-    //inbuilt functions to get the day and day of the week born
-    var bornDay = new Date (birthDate);
-    var dayOfWeek = bornDay.getDay();
 
     //getting display objects for errors
     var firstNameError = document.getElementById("firstNameError"); //atleast one name is required.
@@ -31,42 +25,37 @@ function getAkanName(){
     var userGenderError = document.getElementById("userGenderError");
 
     //final display after submit
-    var AkanNameResult = document.getElementById("AkanNameResult")
-    var valid = false;
-
-    //button event
-    submitbtn.onclick = (event) => {
-        event.preventDefault();
-    }
+    var akanNameResult = document.getElementById("akanNameResult");
 
     //conditions for the required output
-    if (birthDate===""){
-        birthDateError.innerHTML = "Please enter a valid date of birth ";
-        return false;
+    if(!firstName) {
+        // firstNameError.innerHTML = "First Name is required";
+        alert("First Name is required");
+        return;
     }
-    else {
-        for (var i=0;i<userGender.length;i++){
-            if (userGender[i].checked) {
-                valid = true
-            break;
-            }
-        }
-        if (valid){
-            if (userGender[i].value === "Male"){
-                AkanNameResult.innerHTML = "Hello! "+firstName+ +lastName+  "You were born on, "+days[dayOfWeek]+ "Your Akan Name is: " +maleNames[dayOfWeek];
-             }
-            else {
-            if(userGender[i].value === "Female"){
-                AkanNameResult.innerHTML = "Hello! "+firstName+ +lastName+  "You were born on, "+days[dayOfWeek]+ "Your Akan Name is: " +femNames[dayOfWeek];
-            }
-            }
-        }    
-            else {
-                userGenderError.innerHTML = "Please choose your gender";
-                return false;
-            }       
-        
+    
+    if (!birthDate){
+        // birthDateError.innerHTML = "A valid date of birth is required ";
+        alert("A valid date of birth is required");
+        return;
+    }
+   
+    if(!userGender){
+        // userGenderError.innerHTML = "Gender is required"
+        alert("Gender is required");
+        return;
     } 
+
+    var bornDay = new Date (birthDate);
+    var dayOfWeek = bornDay.getDay();
+    console.log("bornDay", bornDay.getDay())
+
+
+    if(userGender === 'Male') {
+        akanNameResult.innerHTML = `Hello! ${firstName} ${lastName || ''} </br> You were born on, ${days[dayOfWeek]} Your Akan Name is:  ${maleNames[dayOfWeek]}`;
+    } else {
+        akanNameResult.innerHTML = `Hello! ${firstName} ${lastName || ''} </br> You were born on, ${days[dayOfWeek]} Your Akan Name is:  ${femNames[dayOfWeek]}`;
+    }
 }
 
 //for the user to reset or refresh the form
@@ -74,78 +63,13 @@ function refreshForm(){
     document.getElementById("firstNameError").innerHTML = "";
     document.getElementById("birthDateError").innerHTML= "";
     document.getElementById("userGenderError").innerHTML= "";
-    document.getElementById("AkanNameResult").innerHTML= "";
-
+    document.getElementById("akanNameResult").innerHTML= "";
 }
 
+//To remove event listners
+function removeListeners() {
+    submitButton.removeEventListener('click', getAkanName)
+    refreshButton.removeEventListener('click', refreshForm) 
+}
 
-//     // var firstName =document.getElementById("inputFirstName");
-//     // var lastName =document.getElementById("inputLastName");
-//     // var birthdate = document.getElementById("birthdate");
-
-
-//     // function printResult(event) {
-//     // event.preventDefault()
-//     // console.log(dataForm.elements["firstname"].value)
-
-//     if(!firstName.value){
-//         errorData.firstName = `<p>First name is required </p>`
-//         errorCount = errorCount +1
-//     }
-//     else if(!lastName.value){
-//         errorData.lastName = `<p> Last name is required </p>`
-//         errorCount = errorCount +1
-//     }
-//     else if(!birthdate.value){
-//         errorData.birthdate = `<p>Birth date is required </p>`
-//         errorCount = errorCount +1
-//     }
-//     else if (!myGender.checked){
-//         errorData.birthdate = `<p>Birth date is required </p>`
-//         errorCount = errorCount +1
-//     }
-//         else {
-//            if (myGender.checked) {
-//                 if (myGender.value === "Male"){
-//                    let AkanName = document.getElementById("alertMessage").innerHTML =  "Hello!: "+firstName+ +lastName+ "You were born on,: "+dayOfWeek+ "Your Akan Name is: " +maleNames[dayOfWeek]
-//                     console.log(AkanName)
-//                 }
-//                 else {
-//                     let AkanName = document.getElementById("alertMessage").innerHTML =  "Hello!: "+firstName+ +lastName+ "You were born on,: "+dayOfWeek+ "Your Akan Name is: " +femNames[dayOfWeek]
-//                     console.log(AkanName)
-//                 }
-                
-//            }    
-//             else {
-//                 document.getElementById("alertMessage") = "Oops! You need to select your gender to continue."
-//             }
-//         } 
-//     }
-
-//     const sbtn = document.getElementById("submitbtn")
-//         sbtn.addEventListener("click",getAkanName())
-//         console.log(sbtn)
-
-
-// //     console.log( birthdate.value)
-// //     errors.innerHTML = `${errorData.firstName} ${errorData.lastName} ${errorData.birthdate}`
-// //     console.log(errorCount,errorData)
-
-
-// // // const sbtn = document.getElementById("submitbtn")
-// // // console.log(sbtn)
-// // // sbtn.addEventListener("click",printResult)
-
-
-
-// // // submitbtn.addEventListener('click'() => {
-// // //     document.getElementById(message).innerHTML
-// // // }
-
-// // // var message = "Hello!: "+firstName+ +lastName+
-// //     // "</br> You were born in: "+dayOfWeek+
-// //     // "</br>Your Akan name is: " +AkanName+
-// //     // document.getElementById('placeholder').innerHTML == message;
-
-// //     // const birthDate = birthdate.value.split("-")
-// //         // console.log(birthDate,birthDate[0], birthDate[1], birthDate[2]}
+window.addEventListener('unload',removeListeners)
